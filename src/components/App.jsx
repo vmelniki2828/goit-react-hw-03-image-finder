281879b36cfcc8375dff2961f4a301025eb117b5
vmelniki2828 <vmelniki2828@gmail.com>
2023-07-13 04:36:37 +0300

    edit

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,9 +3,9 @@ import fetchPictures from './Api';
 import Searchbar from './Searchbar';
 import ImageGallery from './ImageGallery';
 import ImageGalleryItem from './ImageGalleryItem';
-import Loader from './Loader';
-import Button from './Button';
-import Modal from './Modal';
+// import Loader from './Loader';
+// import Button from './Button';
+// import Modal from './Modal';
 import styles from './App.module.css';
 
 export class App extends Component {
